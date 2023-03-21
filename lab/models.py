276b094e0c3574c8cb05a4523af719d774e7b588
aml1 <antonomisha90@gmail.com>
2023-03-21 22:03:38 +0300

    Create all model

--- lab/models.py
+++ lab/models.py
@@ -4,7 +4,27 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Lab(models.Model):
-    people = models.ForeignKey(User, on_delete=models.CASCADE)
+    people = models.ForeignKey(User, on_delete=models.CASCADE, name="Люди в лаборатории")
+    temperature = models.CharField(max_length=3, name="Температура")
+    humidity = models.CharField(max_length=5, name="Влажность")
+    heighWaterFirst = models.CharField(max_length=3, name="Объем в первой колбе")
+    heighWaterSecond = models.CharField(max_length=3, name="Объем во второй колбе")
+    lux = models.CharField(max_length=5, name="Яркость света")
+    co2 = models.CharField(max_length=5, name="Уровень углекислого газа")
+    emission_pr = models.CharField(max_length=5, name="Уровень других газов")
+    countWater = models.CharField(max_length=3, name='Сколько воды прошло')
+    pumpAlert = models.BooleanField(default=False, name="Переполнение, тревога")
+    otherGasAlert = models.BooleanField(default=False, name="Тревога всех газов")
+    co2Alert = models.BooleanField(default=False, name="Углекислый гах, тревога")
+    door = models.BooleanField(default=False, name="Дверь")
+    start_lab = models.BooleanField(default=False, name="Работает ли лаборатория")
+    went = models.BooleanField(default=False, name="Работает ли вентилятор")
+    window = models.BooleanField(default=False, name="Окно")
+
+
+    RGB = models.CharField(max_length=11, name="РГБ, значение жидкости")
+    color = models.CharField(max_length=6, name="Цвет жидкости")
+
 
     def __str__(self):
         return f"Лаборатория номер {str(self.pk)}"
--- lab/views.py
+++ lab/views.py
@@ -5,8 +5,7 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import *
-
-
+import socket
 class ProfileView(APIView):
     permission_classes = [IsAuthenticated]
 
